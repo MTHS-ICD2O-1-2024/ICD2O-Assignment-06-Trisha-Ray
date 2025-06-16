b34cfeb7ce290e996adf165f8481aeb4d1e265ca
Trisha-Ray <trisha.ray@stu.ocsb.ca>
2025-06-16 20:33:31 +0000

    change api fully

--- js/script.js
+++ js/script.js
@@ -6,22 +6,33 @@
 "use strict"
 
 /**
- * This function gets a random dog fact from the API
+ * This function gets a random dof  video from the API
  */
-async function getDogFact() {
+async function getDogImage() {
   try {
     const corsProxy = "https://corsproxy.io/?"
-    const apiUrl = "https://some-random-api.ml/facts/dog"
+    const apiUrl = "https://random.dog/woof.json"
 
     const response = await fetch(corsProxy + encodeURIComponent(apiUrl))
     const data = await response.json()
     console.log(data)
 
-    document.getElementById("dog").innerHTML =
-      '<b>Random Dog Fact:</b><br />' + data.fact
+    const url = data.url
+    const extension = url.split(".").pop().toLowerCase()
+
+    let output = "<b>Here's a random dog!</b>"
+
+    if (["mp4", "webm"].includes(extension)) {
+      output += `<video controls autoplay muted loop width="400">
+                  <source src="${url}" type="video/${extension}">
+                  </video>`
+    } else {
+      output += `<img src="${url}" width="400" />`
+    }
+
+    document.getElementById("dog").innerHTML = output
   } catch (error) {
     console.error(error)
-    document.getElementById("dog").innerHTML =
-      "Could not load dog fact."
+    document.getElementById("dog").innerHTML = "Could not load dog image."
   }
 }
